d72490ee3301bdf04d46a322f89b5c07ff1a6b9f
Mertcan Öncül <mertcanoncul@Mertcan-MacBook-Air.local>
2024-10-11 22:37:30 +0300

    customizing shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 "use client"
+
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
-    <div>
-      Hello World!
-      <Button
-        onClick={() => {
-          alert("hello")
-        }}
-      >
-        Click me
-      </Button>
+    <div className="flex gap-4 m-4 items-center">
+      <Input placeholder="Input" />
+      <Button>Button</Button>
+      <Button variant="destructive">Button</Button>
+      <Button variant="ghost">Button</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="teritary">Teritary</Button>
+      <Button variant="secondary">Button</Button>
     </div>
   )
 }
