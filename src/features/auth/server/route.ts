b0db26354a3526287269b274224a0c20ef1ad047
Mertcan Öncül <mertcanoncul@Mertcan-MacBook-Air.local>
2024-10-12 03:11:29 +0300

    Setting up Appwrite database

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,21 +1,52 @@
 import { Hono } from "hono"
 import { zValidator } from "@hono/zod-validator"
 import { signInFormSchema, signUpFormSchema } from "../schemas"
+import { createAdminClient } from "@/lib/appwrite"
+import { ID } from "node-appwrite"
+import { deleteCookie, setCookie } from "hono/cookie"
+import { AUTH_COOKIE } from "../constants"
 
 const app = new Hono()
   .post("/signin", zValidator("json", signInFormSchema), async (c) => {
     const { email, password } = c.req.valid("json")
 
-    console.log(email, password)
+    const { account } = await createAdminClient()
 
-    return c.json({ email, password })
+    const session = await account.createEmailPasswordSession(email, password)
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    })
+
+    return c.json({ success: true })
   })
   .post("/signup", zValidator("json", signUpFormSchema), async (c) => {
     const { name, email, password } = c.req.valid("json")
 
-    console.log(name, email, password)
+    const { account } = await createAdminClient()
 
-    return c.json({ name, email, password })
+    await account.create(ID.unique(), email, password, name)
+
+    const session = await account.createEmailPasswordSession(email, password)
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    })
+
+    return c.json({ success: true })
+  })
+  .post("/signout", async (c) => {
+    deleteCookie(c, AUTH_COOKIE)
+
+    return c.json({ success: true })
   })
 
 export default app
